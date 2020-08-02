46f839392a58bab73ff95d93aed3095bbf1fbf1a
Divay Kumar <49233122+09programmer@users.noreply.github.com>
2020-08-02 12:32:39 +0530

    Add files via upload

---
+++ happyfriendshipday.py
@@ -0,0 +1,21 @@
+########## 09programmer wishes you a very happy friendship day ##########
+from time import sleep as s
+x,y = "Friends","Forever"
+friends=['Dixit','Rahul','Himanshi','Ria','Parul']
+for friend in friends:
+    print('\/'*9+'\\'+friend+"\/"*10+'\...');s(.2)
+    print("\n\t     "+x+"\t  "+y),s(.2)
+    print("\t"+x+y+"  "+x+y),s(.2)
+    print("    "+(x+y)*2+x*1),s(.2)
+    print(" "*4+(x+y)*2+x*1+y[:1]),s(.2)
+    print(" "*5+x+"happy"+x+"hipDay"+x),s(.2)
+    print("    "+x+y+friend+y+x[:11-len(friend)]),s(.2)
+    print("\t"+(x+y)*2),s(.2)
+    print("\t"+" "*2+x+y+x+y[:2]),s(.2)
+    print("\t"+" "*4+x+y+x[:5]),s(.2)
+    print("\t"+" "*7+x+y),s(.2)
+    print("\t"+" "*10+x),s(.2)
+    print("\t"+" "*13+"v\n"),s(.2)
+    print("\t "+"Happy "+x+"hip Day "+friend+"\n\n\n"),s(.5)
+
+########## follow @09programmer on facebook ##########    
